e2a567f79eda5730bcac875846a1bee34b088948
CarlosDubon <carlosminerodubon@gmail.com>
2015-06-02 15:28:02 -0600

    nav bar l usuario

--- vistas/perfilUsuario.php
+++ vistas/perfilUsuario.php
@@ -1,67 +1,50 @@
-
 <div class="login-form" id="perfil">
-    <div class="panel panel-danger" id="fieldsP">
+    <div class="panel panel-danger">
           <div class="panel-heading">
             <div class="row"> 
                   <div class="col-xs-6 col-md-3">
-                   <a href="#" class="thumbnail" id="photot" >
-                      <img src="{{photo}}" alt="..." class="imagenP" >
-                    </a>
-                  </div>
+                       <a href="" class="thumbnail" id="photot" data-toggle="modal" data-target="#myModal">
+                          <img src="{{photo}}" alt="User Avatar" class="img-circle" id="foto-perfil">
+                        </a>
                 </div>
           </div>
           <div class="panel-body">
-            <form action="./subirFotoUsuario.php"  method="POST" enctype="multipart/form-data">
-           <center> <input type="file" class="btn btn-danger" name="file" id="btnfoto"><br>
-            <button type="submit" class="btn btn-danger"><i class="fa fa-upload"></i></button></center>
+            <form action="./subirFoto.php"  method="POST" enctype="multipart/form-data">
+            <input type="file" class="btn btn-info" name="file" id="btnfoto"><br>
+            <button type="submit" class="btn btn-info" id="btnfile"><i class="fa fa-upload"></i></button>
             </form>
         </div>
     </div>
-         <div class="panel panel-danger" id="fieldsPD">
-          <div class="panel-heading">
-            <h3 class="panel-title">DUI</h3>
-          </div>
-          <div class="panel-body">
-              {{DUI}}
-          </div>
-    </div>
-    <div class="panel panel-danger" id="fieldsPD">
-          <div class="panel-heading">
-            <h3 class="panel-title">User</h3>
-          </div>
-          <div class="panel-body">
-          {{Usuario}}
         </div>
-    </div>
 
-        <div class="panel panel-danger" id="fieldsPD">
-          <div class="panel-heading">
-            <h3 class="panel-title">Name</h3>
-          </div>
+    <div class="panel panel-default">
+            <div class=" panel-heading">
+                General Information
+            </div>
+    </div>
+        <div class="panel panel-default" id="az">
           <div class="panel-body">
-              {{Nombre}}
-    </div>
-    </div>
-    <div class="panel panel-danger" id="fieldsPD">
-          <div class="panel-heading">
-            <h3 class="panel-title">Surname</h3>
-          </div>
-          <div class="panel-body">
-            {{Apellido}}
+            <b>User:</b> {{Usuario}}<br>
+            <b>DUI:</b> {{DUI}}<br>
+            <b>First Name:</b> {{Nombre}}<br>
+              <b>Surname: </b> {{Apellido}}<br>
+            <b>E-mail:</b> {{Correo}}<br>
           </div>
     </div>
-        <div class="panel panel-danger" id="fieldsP">
-          <div class="panel-heading">
-            <h3 class="panel-title">E-mail</h3>
-          </div>
-          <div class="panel-body">
-            {{Correo}}
-          </div>
-    </div>
-
-
     
-    
-    
+        </div>
 
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">{{Usuario}}</h4>
+      </div>
+      <div class="modal-body">
+        <img src="{{photo}}" id="imgc">
+      </div>
+    </div>
+  </div>
 </div>
+
