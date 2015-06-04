e18a06cb2c6db4f96118e83d9f7d7613116ab27f
CarlosDubon <carlosminerodubon@gmail.com>
2015-06-03 22:25:05 -0600

    gfsd

--- clases/Portafolio.php
+++ clases/Portafolio.php
@@ -99,6 +99,13 @@ var $rutaServidor='C:\\xampp\\htdocs\\WorkNetIngles\\portafolio\\';
         
         $encabezado = array('<i class="fa fa-info"></i> ID','<i class="fa fa-file-text-o"></i> File');
         $acciones = '<a href="../portafolio/'.$nombreUsuario[0]['Usuario'].'/{{id}}" ><center><i class="fa fa-download"></i></a>';
+        $acciones .= '<center><div class="ec-stars-wrapper">
+                    <a href="#" data-value="1" title="Votar con 1 estrellas">&#9733;</a>
+                    <a href="#" data-value="2" title="Votar con 2 estrellas">&#9733;</a>
+                    <a href="#" data-value="3" title="Votar con 3 estrellas">&#9733;</a>
+                    <a href="#" data-value="4" title="Votar con 4 estrellas">&#9733;</a>
+                    <a href="#" data-value="5" title="Votar con 5 estrellas">&#9733;</a>
+                    </div>';
 
         $variables['listaArchivos'] = $utilidades->convertirTabla($resultado, $encabezado, $acciones);
         
