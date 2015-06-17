277eba4978a6ec9275de63f9606e076960f2151c
JRjunior32 <junior.rod78@gmail.com>
2015-06-17 11:23:24 -0600

    No había tantos errores

--- vistas/comentarioPub.php
+++ vistas/comentarioPub.php
@@ -1,6 +1,6 @@
 <div class="panel panel-success">
   <div class="panel-heading">
-    <h3 class="panel-title">Publicacion</h3>
+    <h3 class="panel-title">Publication</h3>
   </div>
   <div class="panel-body">
       {{publicacion}}
