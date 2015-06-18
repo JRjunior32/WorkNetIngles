3d9bf7af545051a6fd8a93deed7dbf824790e980
CarlosDubon <carlosminerodubon@gmail.com>
2015-06-17 20:29:28 -0600

    p

--- vistas/plantilla/planti.php
+++ vistas/plantilla/planti.php
@@ -19,17 +19,17 @@
                 <div class="col-md-4">
                     
                                 
-<a href='../controladores/perfil_Mostrar.php' class='list-group-item active'>Perfil<br>    </a>
-<a href="../controladores/mensajes.php" class="list-group-item"><span class="fui-chat"></span>Mensajes</a>
-<a href="#" class="list-group-item"><span class="fui-video"></span>Videoconferencia</a>
-<a href="../controladores/creartrabajador.php" class="list-group-item"><span class="fui-plus"></span>Crear trabajador</a>
-<a href="../controladores/usuarios_empre.php" class="list-group-item"><span class="fui-user"></span>Cuentas</a>
-<a href="../controladores/formEventos.php" class="list-group-item"><span class="fui-calendar-solid"></span>Crear evento</a>
-<a href="../vistas/calendarioEmpresa.php" class="list-group-item"><span class="fui-calendar-solid"></span>Calendario</a>
-<a href="../controladores/crearPortafolio.php" class="list-group-item"><span class="fui-clip"></span>Portafolio</a>
-<a href="../controladores/formOfertas.php" class="list-group-item"><span class="fui-document"></span>Ofertas de empleo</a>
-<a href="../controladores/ofertas_empre.php" class="list-group-item"><span class="fui-document"></span>Mostrar ofertas</a>
-<a href="../controladores/logout.php" class="list-group-item"><span class="fui-power"></span>Cerrar Sesión</a>                  
+<a href='../controladores/perfil_Mostrar.php' class='list-group-item active'>Profile<br>    </a>
+<a href="../controladores/mensajes.php" class="list-group-item"><span class="fui-chat"></span>Messages</a>
+<a href="#" class="list-group-item"><span class="fui-video"></span> Video Calls</a>
+<a href="../controladores/creartrabajador.php" class="list-group-item"><span class="fui-plus"></span>Create a Worker</a>
+<a href="../controladores/usuarios_empre.php" class="list-group-item"><span class="fui-user"></span>Accounts</a>
+<a href="../controladores/formEventos.php" class="list-group-item"><span class="fui-calendar-solid"></span>Create Events</a>
+<a href="../vistas/calendarioEmpresa.php" class="list-group-item"><span class="fui-calendar-solid"></span>Events</a>
+<a href="../controladores/crearPortafolio.php" class="list-group-item"><span class="fui-clip"></span>Portfolio</a>
+<a href="../controladores/formOfertas.php" class="list-group-item"><span class="fui-document"></span>Create Job Offers</a>
+<a href="../controladores/ofertas_empre.php" class="list-group-item"><span class="fui-document"></span> Job Offers</a>
+<a href="../controladores/logout.php" class="list-group-item"><span class="fui-power"></span> Log out</a>                  
                 </div>
 
 </body>
