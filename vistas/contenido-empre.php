adcd84831555a7c1c88f10ee83d0844239f9e276
José Sibrián <atilio.garcia_96@hotmail.com>
2015-07-08 11:29:38 -0600

    jajajjaja

--- vistas/contenido-empre.php
+++ vistas/contenido-empre.php
@@ -1,17 +1,17 @@
      
 <div class="panel panel-warning" id="newsfeed2">
   <div class="panel-heading">
-    <h3 class="panel-title">Noticias</h3>
+    <h3 class="panel-title">News</h3>
   </div>
   <div class="panel-body">
     <div class="form-group">
         <form action="../controladores/guardarPub.php" method="POST">
                 <div class="form-group">
-                  <label class="control-label">Escribe algo</label>
+                  <label class="control-label">Type something...</label>
                   <div class="input-group">
                       <textarea type="text" name="texto" class="form-control" id="publicacion" required></textarea>
                     <span class="input-group-btn">
-                      <input class="btn btn-default" value="Compartir" id="publicacionbtn" type="submit" >
+                      <input class="btn btn-default" value="Share" id="publicacionbtn" type="submit" >
                     </span>
                   </div>
                 </div>        
@@ -25,7 +25,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Comentarios</h4>
+        <h4 class="modal-title" id="myModalLabel">Comments</h4>
       </div>
       <div class="modal-body">
           {{comentarios}}
@@ -33,10 +33,10 @@
             <form action="../controladores/guardarComentario.php" method="POST">
         <div class="modal-footer">
         <div class="form-group">
-          <label class="control-label" id="iz">Comentario</label>
+          <label class="control-label" id="iz">Comment</label>
           <input class="form-control input-lg" type="text" name="comentario" id="inputLarge">
         </div>
-        <input type="submit" class="btn btn-primary" value="Comentar">
+        <input type="submit" class="btn btn-primary" value="Comment">
       </div>
         </form>
 
--- vistas/fragmento/navbar-empre.php
+++ vistas/fragmento/navbar-empre.php
@@ -17,18 +17,18 @@
                     <button type="submit" class="btn btn-default"><span class="fui-search"></span></button>
                 </form>
                 <li class="active"><a href="#"></a></li>
-                <li><a href="../controladores/publicar.php"><span class="fui-home"></span> Inicio</a></li>                
-                <li><a href="./perfil_Mostrar.php"><span class="fui-user"></span>Perfil</a></li>                
+                <li><a href="../controladores/publicar.php"><span class="fui-home"></span> Home</a></li>                
+                <li><a href="./perfil_Mostrar.php"><span class="fui-user"></span> Profile</a></li>                
                 <li class="dropdown">
-                    <a href="" class="dropdown-toggle" data-toggle="dropdown"><span class="fui-gear"></span> Cuenta<b class="caret"></b></a>
+                    <a href="" class="dropdown-toggle" data-toggle="dropdown"><span class="fui-gear"></span> Account<b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li><span class="help-block" id="mover"><i class="fa fa-search"></i> Buscar amigos</span></li>
+                        <li><span class="help-block" id="mover"><i class="fa fa-search"></i> Search Friends</span></li>
                         <li class="divider"></li>
-                        <li><a href="../controladores/buscarempresas.php"><i class="fa fa-building-o"></i> Empresas</a></li>
-                        <li><a href="../controladores/buscarusuarios.php "><i class="fa fa-users"></i> Usuarios</a></li>
+                        <li><a href="../controladores/buscarempresas.php"><i class="fa fa-building-o"></i> Enterprise</a></li>
+                        <li><a href="../controladores/buscarusuarios.php "><i class="fa fa-users"></i> Users</a></li>
                         <li class="divider"></li>
-                        <li><a href="../controladores/crearformCambiarcontra-Empre.php"><span class="fui-gear"></span> Cambiar Contraseña</a></li>
-                        <li><a href="logout.php"><span class="fui-power"></span> Cerrar sesión</a></li>
+                        <li><a href="../controladores/crearformCambiarcontra-Empre.php"><span class="fui-gear"></span> Change Password</a></li>
+                        <li><a href="logout.php"><span class="fui-power"></span> Log Out</a></li>
 
                     </ul>
                 </li>
