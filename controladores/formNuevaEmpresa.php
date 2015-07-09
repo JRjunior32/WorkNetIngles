4d8dd82e63bcdc3029085b51878fca5b76e0ed12
CarlosDubon <carlosminerodubon@gmail.com>
2015-07-08 20:29:43 -0600

    quitar archivos

--- controladores/formNuevaEmpresa.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once realpath(dirname(__FILE__) . '/../clases/AdministrarUsuarios.php');
-require_once realpath(dirname(__FILE__) . '/../clases/Empresa.php');
-$empresa = new Empresa();
-$empresa->mostrarFormulario();
--- controladores/formRecuperar.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once realpath(dirname(__FILE__) . '/../clases/reiniciarPass.php');
-
-$recuperar = new reiniciarPass();
-$recuperar->mostrarForm();
