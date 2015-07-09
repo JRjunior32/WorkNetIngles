ed4cac97ae403f2a52599ae88d786906127f211b
CarlosDubon <carlosminerodubon@gmail.com>
2015-07-08 21:11:43 -0600

    vale verga la puta vida

--- controladores/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once realpath (dirname (__FILE__).'/../clases/AdministrarUsuarios.php');  
-$administrarUsuario = new AdministrarUsuarios();
-$administrarUsuario->logout();
-?>
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*require_once realpath (dirname (__FILE__).'/./clases/Utilidades.php');  
-$utilidades = new Utilidades();
-$utilidades->redireccionar('controladores/index.php');*/
-header('location: controladores/index.php');
-?>
