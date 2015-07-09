af330117978957927d77e818fb2621de0c1ce5b7
CarlosDubon <carlosminerodubon@gmail.com>
2015-07-09 15:11:59 -0600

    fdsfsad

--- vistas/listaOfertasUsuario.php
+++ vistas/listaOfertasUsuario.php
@@ -21,7 +21,7 @@
           <input type="hidden" value="{{idEmpresa}}" name="idEmpresa">
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
         <input type="submit" class="btn btn-primary" value="Aceptar">
       </div>
     </form>
