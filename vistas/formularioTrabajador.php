7c405a9a65f3d3fa65e51a6ad3de43c17be5ea88
CarlosDubon <carlosminerodubon@gmail.com>
2015-07-09 11:45:16 -0600

    correcciones

--- vistas/formularioTrabajador.php
+++ vistas/formularioTrabajador.php
@@ -33,7 +33,7 @@
             </div>
             <div class="form-group">
                 <label for="DUI">DUI:</label>
-                <input type="text" maxlength="10" class="form-control login-field" id="num" onkeydown="return validarNumeros(event)" name="dui" placeholder="DUI" required />
+                <input type="text" maxlength="10" class="form-control login-field" id="num" onkeyup="mascara(this,'-',patron2,true)" onkeydown="return validarNumeros(event)" name="dui" placeholder="DUI" required />
             </div>
             <div class="form-group">
                 <p class="text-center">
@@ -84,4 +84,43 @@
 		return patron.test(te); // prueba
 	}
 </script>
-
+<script>
+    var patron3 = new Array(4,4);
+    var patron2 = new Array(8,1);
+        function mascara(d,sep,pat,nums){
+            if(d.valant != d.value){
+                val = d.value
+                largo = val.length
+                val = val.split(sep)
+                val2 = ''
+                for(r=0;r<val.length;r++){
+                    val2 += val[r]	
+                }
+                if(nums){
+                    for(z=0;z<val2.length;z++){
+                        if(isNaN(val2.charAt(z))){
+                            letra = new RegExp(val2.charAt(z),"g")
+                            val2 = val2.replace(letra,"")
+                        }
+                    }
+                }
+                val = ''
+                val3 = new Array()
+                for(s=0; s<pat.length; s++){
+                    val3[s] = val2.substring(0,pat[s])
+                    val2 = val2.substr(pat[s])
+                }
+                for(q=0;q<val3.length; q++){
+                    if(q ==0){
+                      val = val3[q]
+                    }else{
+                        if(val3[q] != ""){
+                            val += sep + val3[q]
+                        }
+                    }
+                }
+                    d.value = val
+                    d.valant = val
+            }
+}
+</script>
